a0490e9bd92c00211d8afa4133e1956867760156
luongcongduy97 <duylcong97@gmail.com>
2024-10-23 21:33:20 +0700

    5 update route

--- routes/web.php
+++ routes/web.php
@@ -13,28 +13,8 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-Route::get('/tasks/create', [TaskController::class, 'create'])
+Route::resource('tasks', TaskController::class)
     ->middleware(['auth'])
-    ->name('tasks.create');
-
-Route::post('/tasks', [TaskController::class, 'store'])
-    ->middleware(['auth'])
-    ->name('tasks.store');
-
-Route::get('/tasks', [TaskController::class, 'index'])
-    ->middleware(['auth'])
-    ->name('tasks.index');
-
-Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])
-    ->middleware(['auth'])
-    ->name('tasks.edit');
-
-Route::put('/tasks/{task}', [TaskController::class, 'update'])
-    ->middleware(['auth'])
-    ->name('tasks.update');
-
-Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])
-    ->middleware(['auth'])
-    ->name('tasks.destroy');
+    ->except(['show']);
 
 require __DIR__ . '/auth.php';
